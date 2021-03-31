6c596fd23e6893281253e90874d6c30288509ffa
Manuel Lopez <lopez1manuel@gmail.com>
2021-03-30 20:00:21 -0700

    Got Images to appear but not disappear

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
 $(document).ready(function() {
-
-    var url = "https://pokeapi.co.api/v2/pokemon";
-    
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(150));
+        // Create HTML
+    var htmlImages = "";
+    var htmlInfo = "";
+    function createHtmlImg(x) {
+        htmlImages += "<img src=" + x + " alt='characterImg'>";
+        $("#imageSquare").append(htmlImages);
     };
+    function createHtmlInfo(x) {
+        htmlInfo = "<p class='p-2 text-light'><strong>" + x + "</strong></p>";
+        $("#description").append(htmlInfo);
+    };
+        // function that spits out random numbers 1-150
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    };
+    console.log(getRandomInt(150));
     var rand = getRandomInt(150);
-
-    $("#getPoke").on("click", function () {
-        var num = 1;
-        var htmlStr = "";
-
-        $.get(`https://pokeapi.co/api/v2/pokemon${num}`, function (res) {
+    var num = 1;
+        
+        // First Load page with first pokemon
+    $.get(`https://pokeapi.co/api/v2/pokemon/1/`, function (res) {
+            createHtmlImg(res.sprites.front_default);
+            createHtmlInfo(res.name);
+    }, 'json');
+    
+    $(document).on("click", function() {
+           // On Clicking of the button go to the next pokemon up, while grabbing the sprite and info
+        $("#getPoke").on("click", function () {
             num ++;
-            htmlStr += `<img src=${res.sprites.front_default} alt='pokemon_sprite'>`;
+            $("#imageSquare").empty();
+            $("#description").empty();
 
-            $("#imageSquare").append(html_Str);
+            $.get(`https://pokeapi.co/api/v2/pokemon/${num}/`, function (res) {
+                createHtmlImg(res.sprites.front_default);
+                createHtmlInfo(res.name);
+            }, 'json');
+        });
 
-        }, 'json');
-    });
+            // Grab a random pokemon when clicking on getRand Button
+        $("#getRand").on("click", function () {
+            $("#imageSquare").empty();
+            $("#description").empty();
 
-    $("#getRand").on("click", function () {
-        $.get(`url${rand}`, function(kol) {
-            html_Str += `<h1>HelloThere!</h1>`;
-        }, 'json');
-        $("#description").append(html_Str);
-    });
+            $.get(`https://pokeapi.co/api/v2/pokemon/${rand}/`, function(des) {
+                createHtmlImg(des.sprites.front_default);
+                createHtmlInfo(des.name);
+            }, 'json');
+            
+        });
+
+            // Search for a pokemon
+        $("#searchByName").submit(function() {
+            var name = $("#searchByName").val();
+            $("#imageSquare").empty();
+            $("#description").empty();
+
+            $.get("https://pokeapi.co/api/v2/pokemon/" + name +"/",)
+                createHtmlImg(res.sprites.front_default);
+                createHtmlInfo(res.name);
+            return false;
+        });
+        
+        });
+     
 });
+
