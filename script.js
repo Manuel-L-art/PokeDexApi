5893d7d0b3ad25cbfb62b9c7b71b9e56151f60bc
Manuel Lopez <lopez1manuel@gmail.com>
2021-03-31 00:25:39 -0700

    finished, images are replacing images, name and Weight are being displayed

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     var htmlImages = "";
     var htmlInfo = "";
     function createHtmlImg(x) {
-        htmlImages += "<img src=" + x + " alt='characterImg'>";
+        htmlImages = "<img src=" + x + " alt='characterImg'>";
         $("#imageSquare").html(htmlImages);
     };
-    function createHtmlInfo(y) {
-        htmlInfo = "<p class='p-2 text-light'><strong>" + y + "</strong></p>";
-        $("#description").html(htmlInfo);
+    function createHtmlInfo(y,wei) {
+        htmlInfo = `<p class='p-2 text-light'><strong>${num}</strong></p>`;
+        htmlInfoName =`<p class='p-2 text-light'><strong>${y}</strong></p>`;
+        htmlInfoWeight = `<p class='p-2 text-light'><strong>Weight: ${wei}</strong></p>`;
+        $("#description").append(htmlInfo,htmlInfoName, htmlInfoWeight);
     };
         // function that spits out random numbers 1-150
     function getRandomInt(max) {
@@ -22,7 +24,8 @@ $(document).ready(function() {
         // First Load page with first pokemon
     $.get(`https://pokeapi.co/api/v2/pokemon/${num}/`, function (res) {
             createHtmlImg(res.sprites.front_default);
-            createHtmlInfo(res.name);
+            createHtmlInfo(res.name, res.weight);
+            console.log(res.weight)
             
     }, 'json');
 
@@ -36,9 +39,10 @@ $(document).ready(function() {
         $("#description").empty();
         getRandomInt(150);
         var rand = getRandomInt(150);
+        num = rand;
         $.get(`https://pokeapi.co/api/v2/pokemon/${rand}/`, function(des) {
             createHtmlImg(des.sprites.front_default);
-            createHtmlInfo(des.name);
+            createHtmlInfo(des.name, des.weight);
             console.log(des.name);
         });
         
@@ -53,25 +57,25 @@ $(document).ready(function() {
         $.get(`https://pokeapi.co/api/v2/pokemon/${num}/`, function (yes) {
             
             createHtmlImg(yes.sprites.front_default);
-            createHtmlInfo(yes.name);
+            createHtmlInfo(yes.name, yes.weight);
             // console.log(yes.name)
         }, 'json');
     });
         // Search for a pokemon
-        $("#searchByName").submit(function() {
+        $("form").submit(function() {
             //  var name = $("#searchByName").formInteract();
-            var xy = document.getElementById("form").text();
+            var xy = $("#searchByName").val();
+            console.log(xy);
+            console.log("hello")
             $("#imageSquare").empty();
             $("#description").empty();
-
-              $.get("https://pokeapi.co/api/v2/pokemon/" + xy +"/",function (joy){
+               
+              $.get(`https://pokeapi.co/api/v2/pokemon/${xy}/`,function (joy){
                  createHtmlImg(joy.sprites.front_default);
-                 createHtmlInfo(joy.name);
-                 console.log(xy);
-
+                 createHtmlInfo(joy.name, joy.weight);
                 },'json');
              
-            // return false;
+            return false;
         });
      
 });
